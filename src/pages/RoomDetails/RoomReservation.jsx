176f9db2488a3f05a27fdb4057d36da9f1a40623
Mahin-Hasan <mahinmtrs@gmail.com>
2024-06-01 06:18:38 +0600

    implemented stripe payment method structure with checkout form

--- src/pages/RoomDetails/RoomReservation.jsx
+++ src/pages/RoomDetails/RoomReservation.jsx
@@ -3,9 +3,16 @@ import { formatDistance } from "date-fns";
 import Button from "../../components/Button/Button";
 import Calender from "./Calender";
 import { useState } from "react";
+import BookingModal from "../../components/Modal/BookingModal";
+import useAuth from "../../hooks/useAuth";
 
 const RoomReservation = ({ room }) => {
     let [isOpen, setIsOpen] = useState(false)
+    const { user } = useAuth()
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
     const [value, setValue] = useState({
         startDate: new Date(room?.from),
         endDate: new Date(room?.to),
@@ -18,7 +25,25 @@ const RoomReservation = ({ room }) => {
 
     const totalPrice = totalDays * room?.price
 
-   
+    const [bookingInfo, setBookingInfo] = useState({
+        guest:
+        {
+            name: user?.displayName,
+            email: user?.email,
+            image: user?.photoURL
+        },
+        host: room?.host?.email,
+        location: room?.location,
+        price: totalPrice,
+        to: value.endDate,
+        from: value.startDate,
+        title: room?.title,
+        roomId: room?._id,
+        image: room?.image,
+    })
+
+
+
 
     return (
         <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
@@ -32,13 +57,20 @@ const RoomReservation = ({ room }) => {
             </div>
             <hr />
             <div className="p-4">
-                <Button label={'Reserve'} />
+                <Button onClick={() => setIsOpen(true)} label={'Reserve'} />
             </div>
             <hr />
             <div className="flex items-center justify-between font-semibold text-lg p-4">
                 <div>Total</div>
                 <div>$ {totalPrice}</div>
             </div>
+
+            {/* Modal */}
+            <BookingModal
+                closeModal={closeModal}
+                isOpen={isOpen}
+                bookingInfo={bookingInfo}
+            />
         </div>
     );
 };
